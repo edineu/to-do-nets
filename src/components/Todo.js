1adae537d41ca8c69253e34639863515bb535475
Ed Hansen <ed@neubra.dk>
2021-01-20 15:52:32 +0100

    arrange el and id funct to the component

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,11 +1,16 @@
-const Todo = () => {
+const Todo = ({ text, todos, todo, setTodos }) => {
+  // add Events
+  const deleteHandler = () => {
+    setTodos(todos.filter((el) => el.id !== todos.id));
+  };
+
   return (
-    <div className="tod-do">
-      <li className="todo-items"> HEY</li>
-      <button className="fas fa-check">
-        <i className="complete-btn"></i>
+    <div className="todo">
+      <li className="todo-item">{text}</li>
+      <button className="complete-btn">
+        <i className="fas fa-check"></i>
       </button>
-      <button className="trash-btn">
+      <button onClick={deleteHandler} className="trash-btn">
         <i className="fas fa-trash"></i>
       </button>
     </div>
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,10 +1,20 @@
 //import components
+import Todo from "./Todo";
 
-const TodoList = ({ todos }) => {
+const TodoList = ({ todos, setTodos }) => {
   console.log(todos);
   return (
     <div className="todo-container">
-      <ul className="todo-list"></ul>
+      <ul className="todo-list">
+        {todos.map((todo) => (
+          <Todo
+            setTodos={setTodos}
+            todos={todos}
+            key={todo.id}
+            text={todo.text}
+          />
+        ))}
+      </ul>
     </div>
   );
 };
