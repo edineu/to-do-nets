d4e08699d169727f5367c07c1d463d5182f6019a
Ed Hansen <ed@neubra.dk>
2021-01-20 20:53:21 +0100

    minor error fix

--- src/App.js
+++ src/App.js
@@ -52,23 +52,26 @@ function App() {
   };
 
   return (
-    <div className="App">
-      <header>
-        <h1>Ed's Todo List</h1>{" "}
-      </header>
-      <Form
-        todos={todos}
-        setTodos={setTodos}
-        inputText={inputText}
-        setInputText={setInputText}
-        setStatus={setStatus}
-      />
-      <TodoList
-        filteredTodos={filteredTodos}
-        setTodos={setTodos}
-        todos={todos}
-      />
-    </div>
+    <>
+      {" "}
+      <div className="App">
+        <header>
+          <h1>Ed's Todo List</h1>{" "}
+        </header>
+        <Form
+          todos={todos}
+          setTodos={setTodos}
+          inputText={inputText}
+          setInputText={setInputText}
+          setStatus={setStatus}
+        />
+        <TodoList
+          filteredTodos={filteredTodos}
+          setTodos={setTodos}
+          todos={todos}
+        />
+      </div>
+    </>
   );
 }
 
---
+++ src/components/styles/GlobalStyle.js
@@ -0,0 +1,13 @@
+import { createGlobalStyles } from "styled-components";
+
+const GlobalStyles = createGlobalStyles`
+    * {
+        box-sizing: border-box;
+    }
+    body {
+        font-size: 10px
+        font-family: 'Hind', san-sarif;
+    }
+`;
+
+export default GlobalStyles;
