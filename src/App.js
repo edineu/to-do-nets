37e873430610a24938e893ec8b5eb8f208fa6cfe
Ed Hansen <ed@neubra.dk>
2021-01-20 13:35:07 +0100

    todo list component jsx

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
+import React, { useState } from "react";
 // imported components
 import Form from "./components/Form";
-import "./App.css";
+import TodoList from "./components/TodoList";
 
 function App() {
+  const [inputText, setInput];
   return (
     <div className="App">
       <header>
         <h1>Ed's Todo List</h1>
       </header>
       <Form />
+      <TodoList />
     </div>
   );
 }
